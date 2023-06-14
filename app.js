44317f19081a32b5860c55476dc60ccf818a88bb
vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>
2023-06-14 18:34:43 +0300

    Add function print for callback hell

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const fs = require("fs");
 const filePaths = ["message1.txt", "message2.txt", "message3.txt"];
 
-console.log("Start program");
-
-fs.readFile(filePaths[0], "utf8", (err, message) => {
+function print(err, message) {
   if (err) {
     console.log(err);
   }
   console.log(message);
+}
+
+console.log("Start program");
+
+fs.readFile(filePaths[0], "utf8", (err, message) => {
+  print(err, message);
   fs.readFile(filePaths[1], "utf8", (err, message) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(message);
+    print(err, message);
     fs.readFile(filePaths[2], "utf8", (err, message) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(message);
+      print(err, message);
     });
   });
 });
