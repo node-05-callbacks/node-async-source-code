f4cd9f8998de9127fc6bd833b9685d2ae862be37
vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>
2023-06-14 18:32:51 +0300

    Read async from 3 files and consloe.log in random order

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
-const filePaths = ["message1.txt", "message2.txt", "message3.txt"]
-
+const filePaths = ["message1.txt", "message2.txt", "message3.txt"];
 
 console.log("Start program");
 for (const filePath of filePaths) {
-  let message = fs.readFileSync(filePath, "utf8")
-  console.log(message);
+  fs.readFile(filePath, "utf8", (err, message) => {
+    console.log(message);
+  });
 }
 console.log("End program");
