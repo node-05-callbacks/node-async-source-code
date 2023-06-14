eb48b0aa26f5953208394606601a2f7f24e3d108
vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>
2023-06-14 18:33:50 +0300

    Delete loop for callback hell

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const fs = require("fs");
 const filePaths = ["message1.txt", "message2.txt", "message3.txt"];
 
 console.log("Start program");
-for (const filePath of filePaths) {
-  fs.readFile(filePath, "utf8", (err, message) => {
+
+fs.readFile(filePaths[0], "utf8", (err, message) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(message);
+  fs.readFile(filePaths[1], "utf8", (err, message) => {
+    if (err) {
+      console.log(err);
+    }
     console.log(message);
+    fs.readFile(filePaths[2], "utf8", (err, message) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(message);
+    });
   });
-}
+});
+
 console.log("End program");
